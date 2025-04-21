49f200046260dbb9a4624b632aecc021922b2173
Del1ng <105551592+Del1ng@users.noreply.github.com>
2025-04-21 11:38:23 +0200

    Add files via upload

---
+++ 1.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+
+using namespace std;
+//1.
+//int main() {
+//	cout << "Wait for me, I will return." ;
+//	cout << "\nWait for all you’re worth." ;
+//	cout << "\nWait, when sulphurous rains they burn";
+//	cout << "\nSnuffing out all mirth,";
+//	cout << "\nWait, as drifting snow it drops,";
+//	cout << "\nWait, when heat’s too much.";
+//	cout << "\nWait, when wait for others stops,";
+//	cout << "\nPast has lost its clutch.";
+//	cout << "\nWait, when from that distant land ";
+//	cout << "\nLetters do not come.";
+//	cout << "\nWait, when all are weary and";
+//	cout << "\nLeave the wait to some.";
+//}
+
+//2.
+//int main() {
+//	cout << "Home task #2.2\n\n";
+//	cout << "Every" << "\n";
+//	cout << "\tHunter" << "\n";
+//	cout << "\t\tWants" << "\n";
+//	cout << "\t\t\tTo know" << "\n";
+//	cout << "\t\t\t\t Where" << "\n";
+//}
+
+//3.
+//int main() {
+//	cout << "\t\t-----------------------------------------\n";
+//	cout << "\t\t|\tPuppies for sale.\t\t|\n";
+//	cout << "\t\t|\tBreed: Rottweiler.\t\t|\n";
+//	cout << "\t\t|\tTel.: 22222222222\t\t|\n";
+//	cout << "\t\t|\tName: Max.\t\t\t|";
+//	cout << "\n\t\t|\tAge: 3 months.\t\t\t|";
+//	cout << "\n\t\t|\tColor: Black and brown.\t\t|";
+//	cout << "\n\t\t|\tPrice: $500.\t\t\t|\n";
+//	cout << "\t\t-----------------------------------------\n";
+//}
