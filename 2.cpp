d1223f45a0ee348c09f027a7dac3d3ecae44ea38
Del1ng <105551592+Del1ng@users.noreply.github.com>
2025-04-24 16:59:16 +0200

    Add files via upload

---
+++ 2.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+//1.
+//int main() {
+//    int a, sum = 0;
+//    cout << "Input a: ";
+//    cin >> a;
+//    for (int i = a; i <= 500; i++) {
+//        sum += i;
+//    }
+//    cout << "Sum " << a << " до 500: " << sum << endl;
+//    return 0;
+//}
+
+//2.
+//int main() {
+//    int x, y;
+//    cout << "Enter integer x: ";
+//    cin >> x;
+//    cout << "Enter integer y: ";
+//    cin >> y;
+//    int result = static_cast<int>(pow(x, y)); // Cast to int for whole number result
+//    cout << x << " raised to the power " << y << " is: " << result << endl;
+//    return 0;
+//}
+
+//3.
+//int main() {
+//    int a;
+//    long long product = 1;
+//    cout << "Enter a number a (1 <= a <= 20): ";
+//    cin >> a;
+//    for (int i = a; i <= 20; i++) {
+//        product *= i;
+//    }
+//    cout << "The product of integers from " << a << " to 20 is: " << product << endl;
+//    return 0;
+//}
+
+//4
+//int main() {
+//    int k;
+//    cout << "Enter number k (e.g., your variant number): ";
+//    cin >> k;
+//    for (int i = 2; i <= 10; i++) {
+//        cout << k << " x " << i << " = " << k * i << endl;
+//    }
+//    return 0;
+//}
