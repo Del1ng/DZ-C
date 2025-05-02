324e25782a79b055197fa317a65fefccff2695ad
Del1ng <105551592+Del1ng@users.noreply.github.com>
2025-05-02 19:02:56 +0200

    Add files via upload

--- 4.cpp
+++ 4.cpp
@@ -1,38 +1,35 @@
-﻿#include <iostream>
-#include <cstdlib>   
-#include <ctime>     
+﻿//2.
+#include <iostream>
 using namespace std;
 
+//int main() {
+//    setlocale(LC_ALL, "Russian");
+//    cout << "Введите количество элементов в массиве: ";
+//
+//    int arr[4];
+//
+//    cout << "Введите элементы массива: ";
+//    for (int i = 0; i < 4; i++) {
+//        cin >> arr[i];
+//    }
+//
+//    cout << "Массив в обратном порядке: ";
+//    for (int i = 4 - 1; i >= 0; i--) {
+//        cout << arr[i] << " ";
+//    }
+//
+//    cout << endl;
+//}
+//3.
 int main() {
-	int arr[4][4];
-	int count = 0;
-	for (int i = 0; i < 4; i++)
-	{
-		for (int j = 0; j < 4; j++)
-		{
-				cin >> arr[i][j];
-		}
-		cout << endl;
-	}
-	for (int i = 0; i < 4; i++)
-	{
-		for (int j = 0; j < 4; j++)
-		{
-			cout << arr[i][j] << "  ";
-		}
-		cout << endl;
-	}
-	cout << endl;
-	for (int i = 0; i < 4; i++)
-	{
-		for (int j = 0; j < 4; j++){
-			if (j < i) {
-				if (arr[i][j] == 0) {
-					count++;
-				}
-			}
-		}
-	
-	}
-	cout << "kol nuley =" << count << endl;
+    double sides[5]; 
+    double perimeter = 0;
+    cout << "Введите длины сторон пятиугольника: ";
+    for (int i = 0; i < 5; i++) {
+        cin >> sides[i];
+        perimeter += sides[i];
+    }
+
+    cout << "Периметр пятиугольника: " << perimeter << endl;
+
 }
