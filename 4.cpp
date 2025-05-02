ec25e9e64610f7ac8332d722f63a4ac60d39bbf5
Del1ng <105551592+Del1ng@users.noreply.github.com>
2025-05-02 19:02:30 +0200

    Add files via upload

---
+++ 4.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <cstdlib>   
+#include <ctime>     
+using namespace std;
+
+int main() {
+	int arr[4][4];
+	int count = 0;
+	for (int i = 0; i < 4; i++)
+	{
+		for (int j = 0; j < 4; j++)
+		{
+				cin >> arr[i][j];
+		}
+		cout << endl;
+	}
+	for (int i = 0; i < 4; i++)
+	{
+		for (int j = 0; j < 4; j++)
+		{
+			cout << arr[i][j] << "  ";
+		}
+		cout << endl;
+	}
+	cout << endl;
+	for (int i = 0; i < 4; i++)
+	{
+		for (int j = 0; j < 4; j++){
+			if (j < i) {
+				if (arr[i][j] == 0) {
+					count++;
+				}
+			}
+		}
+	
+	}
+	cout << "kol nuley =" << count << endl;
+}
