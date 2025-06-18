ff6919a7db67239b021bef2dc9f031efad1700b9
Del1ng <105551592+Del1ng@users.noreply.github.com>
2025-06-18 18:38:09 +0200

    Add files via upload

---
+++ 13.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+class Student {
+private:
+    string name;
+    int grade;
+
+public:
+    Student(const string& name, int grade) : name(name), grade(grade) {}
+
+    bool isPassed() const {
+        return grade >= 60;
+    }
+
+    void print() const {
+        cout << name << " — " << grade << endl;
+    }
+};
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+    vector<Student> students = {
+    Student("Алексей", 75),
+    Student("Мария", 58),
+    Student("Иван", 90),
+    Student("Ольга", 45),
+    Student("Дмитрий", 65)
+    };
+
+    cout << "Студенты, которые сдали экзамен:" << endl;
+    for (const auto& student : students) {
+        if (student.isPassed()) {
+            student.print();
+        }
+    }
+
+}
